f6c276e5e0619c9f24531ff8e66d3c1f2ced6e3d
yemy <yemybold@gmail.com>
2026-03-23 23:29:45 +0300

    failed

--- count_css.py
+++
@@ -1,7 +0,0 @@
-import re
-with open('f:/code/web/MMC/frontend/index.html', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-matches = re.finditer(r'href=[\'\"]([^\'\"]+\.css)', text)
-for m in matches:
-    print(m.group(1))
--- web/context_processors.py
+++
@@ -1,9 +0,0 @@
-from .models import SiteSettings, Partner
-
-def site_context(request):
-    settings = SiteSettings.objects.first()
-    partners = Partner.objects.all()
-    return {
-        'site_settings': settings,
-        'all_partners': partners,
-    }
